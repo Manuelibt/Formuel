95d045c4932adc102058f1b6747caeed6e12e611
Manuelibt <Github@manuelrohman.de>
2026-01-20 10:03:03 +0100

    Add honeypot and timing checks to form

--- templates/form.php
+++ templates/form.php
@@ -1,6 +1,7 @@
 <?php
 /** @var array $values */
 /** @var string $message */
+/** @var int $timestamp */
 ?>
 <div class="formuel-wrapper">
     <?php if ($message === 'success') : ?>
@@ -14,6 +15,11 @@
     <?php endif; ?>
 
     <form class="formuel-form" method="post">
+        <div class="formuel-field formuel-field--honeypot" aria-hidden="true">
+            <label for="formuel-hp"><?php echo esc_html__('Leave this field empty', 'formuel'); ?></label>
+            <input type="text" id="formuel-hp" name="formuel_hp" value="" autocomplete="off" tabindex="-1" />
+        </div>
+
         <div class="formuel-field">
             <label for="formuel-name"><?php echo esc_html__('Name', 'formuel'); ?></label>
             <input type="text" id="formuel-name" name="formuel_name" value="<?php echo esc_attr($values['name']); ?>" required />
@@ -29,6 +35,7 @@
             <textarea id="formuel-message" name="formuel_message" rows="5" required><?php echo esc_textarea($values['message']); ?></textarea>
         </div>
 
+        <input type="hidden" name="formuel_time" value="<?php echo esc_attr($timestamp); ?>" />
         <?php wp_nonce_field(Formuel_Shortcode::NONCE_ACTION, 'formuel_nonce'); ?>
         <button class="formuel-button" type="submit" name="formuel_submit" value="1">
             <?php echo esc_html__('Send', 'formuel'); ?>
